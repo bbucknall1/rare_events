af6ee05515a1e3c1ec8f848230157cd4f5ed36ef
ben <bbucknall1@gmail.com>
2021-11-01 14:10:50 +0100

    After first test run of sim file

--- sim1/problem.c
+++ sim1/problem.c
@@ -79,13 +79,6 @@ struct reb_simulation* init_sim(){
     }
     reb_move_to_com(r);
 
-    struct rebx_extras* rebx = rebx_attach(r);
-    // Could also add "gr" or "gr_full" here.  See documentation for details.
-    struct rebx_force* gr = rebx_load_force(rebx, "gr");
-    rebx_add_force(rebx, gr);
-    // Have to set speed of light in right units (set by G & initial conditions).  Here we use default units of AU/(yr/2pi)
-    rebx_set_param_double(rebx, &gr->ap, "c", 10065.32);
-
     return r;
 }
 
@@ -93,17 +86,27 @@ void heartbeat(struct reb_simulation* r){
     if (reb_output_check(r, 10000.)){
         reb_output_timing(r, tmax);
         reb_integrator_synchronize(r);
+        /*
         FILE* f = fopen("energy.txt","a");
         double e = reb_tools_energy(r);
         fprintf(f,"%e %e\n",r->t, fabs((e-e_init)/e_init));
         fclose(f);
+        */
     }
 }
 
 int main(int argc, char* argv[]){
     struct reb_simulation* sim = init_sim();
 
-    double tmax = 5.e-1;
+
+    struct rebx_extras* rebx = rebx_attach(sim);
+    // Could also add "gr" or "gr_full" here.  See documentation for details.
+    struct rebx_force* gr = rebx_load_force(rebx, "gr");
+    rebx_add_force(rebx, gr);
+    // Have to set speed of light in right units (set by G & initial conditions).  Here we use default units of AU/(yr/2pi)
+    rebx_set_param_double(rebx, &gr->ap, "c", 10065.32);
+
+    //double tmax = 5.e-1;
     reb_integrate(sim, tmax);
     rebx_free(rebx);    // this explicitly frees all the memory allocated by REBOUNDx
     reb_free_simulation(sim);
