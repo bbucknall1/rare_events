3ec7c1a534d10485b6aaed0d3dae367517a6ef7e
ben <bbucknall1@gmail.com>
2021-11-05 09:19:19 +0100

    Double checked units in relativistic additions

--- sim1/problem.c
+++ sim1/problem.c
@@ -163,14 +163,14 @@ double gaussian(){
 int main(int argc, char* argv[]){
     struct reb_simulation* sim = init_sim();
 
-    /*
+
     struct rebx_extras* rebx = rebx_attach(sim);
     // Could also add "gr" or "gr_full" here.  See documentation for details.
     struct rebx_force* gr = rebx_load_force(rebx, "gr");
     rebx_add_force(rebx, gr);
     // Have to set speed of light in right units (set by G & initial conditions).  Here we use default units of AU/(yr/2pi)
     rebx_set_param_double(rebx, &gr->ap, "c", 10065.32);
-    */
+    
     //double tmax = 5.e-1;
     reb_integrate(sim, tmax);
     //rebx_free(rebx);    // this explicitly frees all the memory allocated by REBOUNDx
