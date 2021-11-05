ae53ebe2066ff96a693ee866e74eb0d99c6e3114
ben <bbucknall1@gmail.com>
2021-11-05 09:20:49 +0100

    Tidying

--- sim1/problem.c
+++ sim1/problem.c
@@ -2,8 +2,8 @@
  * First attempt at building solar system model with Mercury perturbations at regular intervals
  *
  *  TO DO:
- *    o Change planet locations to astronomical units
- *    o Implement normal distribution generator
+ *    x Change planet locations to astronomical units
+ *    x Implement normal distribution generator
  *    o Add Mercury perturbations to heartbeat function
  *    o Implement sorted stratified resampling method
  *    o Splitting and killing
@@ -24,37 +24,6 @@
 
 #define SOLAR_MASS 1.988544e30     // Solar Mass in kg
 #define AU 149597870700         // Astronomical Unit in m
-
-/*
-double ss_pos[10][3] =          // in AU
-{
-    {3.256101656448802E-03  , -1.951205394420489E-04 , -1.478264728548705E-04},
-    {-1.927589645545195E-01 , 2.588788361485397E-01  , 3.900432597062033E-02 },
-    {-5.976537074581466E-01 , 3.918678996109574E-01  , 3.990356741282203E-02 },
-    {-7.986189029000561E-01 , -6.086873314992410E-01 , -1.250824315650566E-04},
-    {7.897942807177173E-01  , 1.266671734964037E+00  , 7.092292179885432E-03 },
-    {-4.314503046344270E+00 , 3.168094294126697E+00  , 8.331048545353310E-02 },
-    {-4.882304833383455E+00 , -8.689263067189865E+00 , 3.453930436208210E-01 },
-    {1.917757033372740E+01  , 5.671738750949031E+00  , -2.273858614425555E-01},
-    {2.767031517959636E+01  , -1.150331645280942E+01 , -4.008018419157927E-01},
-    {7.765250227278298E+00  , -3.190996242617413E+01 , 1.168394015703735E+00 },
-
-};
-
-double ss_vel[10][3] =        // in AU/day
-{
-    {3.039963463108432E-06 ,  6.030576499910942E-06 ,  -7.992931269075703E-08},
-    {-2.811550184725887E-02,  -1.586532995282261E-02,  1.282829413699522E-03 },
-    {-1.113090630745269E-02,  -1.703310700277280E-02,  4.089082927733997E-04 },
-    {1.012305635253317E-02 ,  -1.376389620972473E-02,  3.482505080431706E-07 },
-    {-1.135279609707971E-02,  8.579013475676980E-03 ,  4.582774369441005E-04 },
-    {-4.555986691913995E-03,  -5.727124269621595E-03,  1.257262404884127E-04 },
-    {4.559352462922572E-03 ,  -2.748632232963112E-03,  -1.337915989241807E-04},
-    {-1.144087185031310E-03,  3.588282323722787E-03 ,  2.829006644043203E-05 },
-    {1.183702780101068E-03 ,  2.917115980784960E-03 ,  -8.714411604869349E-05},
-    {3.112825364672655E-03 ,  1.004673400082409E-04 ,  -9.111652976208292E-04},
-};
-*/
 
 double ss_pos[10][3] =
 {
@@ -170,7 +139,7 @@ int main(int argc, char* argv[]){
     rebx_add_force(rebx, gr);
     // Have to set speed of light in right units (set by G & initial conditions).  Here we use default units of AU/(yr/2pi)
     rebx_set_param_double(rebx, &gr->ap, "c", 10065.32);
-    
+
     //double tmax = 5.e-1;
     reb_integrate(sim, tmax);
     //rebx_free(rebx);    // this explicitly frees all the memory allocated by REBOUNDx
