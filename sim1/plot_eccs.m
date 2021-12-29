5b8b6b79c95fe0ee96d9fbb6f7285f0525a027fc
ben <bbucknall1@gmail.com>
2021-12-29 11:49:06 +0100

    Plotting

--- sim1/plot_eccs.m
+++ sim1/plot_eccs.m
@@ -1,17 +1,15 @@
-eccs0 = readmatrix("sim_0_ecc_dmc.csv");
-eccs1 = readmatrix("sim_1_ecc_dmc.csv");
-eccs2 = readmatrix("sim_2_ecc_dmc.csv");
-eccs3 = readmatrix("sim_3_ecc_dmc.csv");
-
+N = 5;
 smth = 1001;
-
-smoothed0 = movmean(eccs0, smth);
-smoothed1 = movmean(eccs1, smth);
-smoothed2 = movmean(eccs2, smth);
-smoothed3 = movmean(eccs3, smth);
-
 xs = 0:50000/(2*pi):(2e8)+10000;
+L = length(xs);
 
-plot(xs, smoothed0, xs, smoothed1, xs, smoothed2, xs, smoothed3);
-ylim([0 0.5])
-legend(["sim0" "sim1" "sim2" "sim3"]);
+eccs = zeros(N+1, L);
+smoothed = zeros(N+1, L);
+
+hold on;
+for i = 0:N
+    eccs(i+1,:) = readmatrix("sim_"+num2str(i)+"_ecc_dmc.csv");
+    smoothed(i+1,:) = movmean(eccs(i+1,:), smth);
+    plot(xs, smoothed(i+1,:));
+end
+ylim([0 0.5]);
